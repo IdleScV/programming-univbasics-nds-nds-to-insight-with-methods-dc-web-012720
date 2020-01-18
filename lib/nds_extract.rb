5f425a2fc47dfc326b70917c87877694680d3a47
Wesley Chen <weschen1996@gmail.com>
2020-01-18 06:24:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,6 +3,7 @@ require 'directors_database'
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
+
 def gross_for_director(director_data)
   count = 0;
   sum = 0
@@ -17,6 +18,7 @@ end
 # The return value should be like:
 #
 # { directorOne => allTheMoneyTheyMade, ... }
+
 def directors_totals(nds)
   result = {}
   count = 0;
