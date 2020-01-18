ec02a6a7872f7a7c67fad6253cccd8bced5c8035
Wesley Chen <weschen1996@gmail.com>
2020-01-18 06:28:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,7 +5,7 @@ require 'directors_database'
 # using director_data as input
 
 def gross_for_director(director_data)
-  count = 0;
+  count = 0
   sum = 0
   while director_data[:movies][count] do
     sum += director_data[:movies][count][:worldwide_gross]
